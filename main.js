f8ec2e0f0ca403da0aa3d1fd437b85cb7ea9992d
benlevine1 <benlevine1@gmail.com>
2018-11-08 18:25:54 -0800

    Memory Match version0.5-Ben Levine

--- main.js
+++ main.js
@@ -1,87 +1,99 @@
 var cardsArray;
 cardsArray = [
-    {name: 'bluefin', src: "memory%20match%20images/bluefin.jpg"},
-    {name: 'bluefin', src: "memory%20match%20images/bluefin.jpg"},
-    {name: 'bonita', src: "memory%20match%20images/bonita1.jpg"},
-    {name: 'bonita', src: "memory%20match%20images/bonita1.jpg"},
-    {name: 'calico', src: "memory%20match%20images/calico%20bass.jpg"},
-    {name: 'calico', src: "memory%20match%20images/calico%20bass.jpg"},
-    {name: 'halibut', src: "memory%20match%20images/California_halibut.jpg"},
-    {name: 'halibut', src: "memory%20match%20images/California_halibut.jpg"},
-    {name: 'dorado', src: "memory%20match%20images/dorado_t658_t658.jpg"},
-    {name: 'dorado', src: "memory%20match%20images/dorado_t658_t658.jpg"},
-    {name: 'sheephead', src: "memory%20match%20images/sheephead.jpg"},
-    {name: 'sheephead', src: "memory%20match%20images/sheephead.jpg"},
-    {name: 'white seabass', src: "memory%20match%20images/white%20seabass.jpg"},
-    {name: 'white seabass', src: "memory%20match%20images/white%20seabass.jpg"},
-    {name: 'yellowfin', src: "memory%20match%20images/yellowfin.jpg"},
-    {name: 'yellowfin', src: "memory%20match%20images/yellowfin.jpg"},
-    {name: 'yellowtail', src: "memory%20match%20images/Yellowtail.jpg"},
-    {name: 'yellowtail', src: "memory%20match%20images/Yellowtail.jpg"}
+    {name: 'babeRuth', 'img': "memorymatchimages/baberuth.jpg"},
+    {name: 'babeRuth', 'img': "memorymatchimages/baberuth.jpg"},
+    {name: 'honusWagner', 'img': "memorymatchimages/honuswagner.jpg"},
+    {name: 'honusWagner', 'img': "memorymatchimages/honuswagner.jpg"},
+    {name: 'johnnyBench', 'img': "memorymatchimages/johnnybench.jpg"},
+    {name: 'johnnyBench', 'img': "memorymatchimages/johnnybench.jpg"},
+    {name: 'louGehrig', 'img': "memorymatchimages/lougehrig.jpg"},
+    {name: 'louGehrig', 'img': "memorymatchimages/lougehrig.jpg"},
+    {name: 'mikeSchmidt', 'img': "memorymatchimages/mikeschmidt.jpg"},
+    {name: 'mikeSchmidt', 'img': "memorymatchimages/mikeschmidt.jpg"},
+    {name: 'rogerHornsby', 'img': "memorymatchimages/rogerhornsby.jpg"},
+    {name: 'rogerHornsby', 'img': "memorymatchimages/rogerhornsby.jpg"},
+    {name: 'sandyKoufax', 'img': "memorymatchimages/sandykoufax.jpg"},
+    {name: 'sandyKoufax', 'img': "memorymatchimages/sandykoufax.jpg"},
+    {name: 'tedWilliams', 'img': "memorymatchimages/tedwilliams.jpg"},
+    {name: 'tedWilliams', 'img': "memorymatchimages/tedwilliams.jpg"},
+    {name: 'willieMays', 'img': "memorymatchimages/williemays.jpg"},
+    {name: 'willieMays', 'img': "memorymatchimages/williemays.jpg"},
 ];
 
-cardArrayShuffled =
+var newDeck = shuffleArray(cardsArray);
 
 $(document).ready(readySetGo);
 
 // front.css("background-image",)
 function readySetGo(){
-    createCards()
+    shuffleArray(cardsArray);
+    createCards(newDeck);
+    toggleCard();
 }
 
-function createCards() {
+function shuffleArray(array) {
+    var j, x, i;
+    for (i = array.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = x;
+    }
+    return array
+}
+
+function createCards(shuffledArray) {
     for (x = 0; x < cardsArray.length; x++){
         var cardContainer = $('<div>',{
             'class': "card-container"
         });
-        var front = $('<div>', {
-            "class": "card-front",
-            'display': 'none'
-        });
-        var back = $('<div>', {
-            'class': 'card-back',
-            'background-image': cardsArray[x].src,
-            'background-repeat': 'no-repeat',
-            'background-size': 'contain',
-            // 'display': 'none'
+        var frontImage = $("<img>", {
+            'class': 'card',
+            src: 'memorymatchimages/baseballfrontpicture.jpg',
+            click: toggleCard
+            });
+        var backImage = $('<img>', {
+            'class': 'card',
+            src: shuffledArray[x].img,
+            click: toggleCard
         });
         $('.game-board').append(cardContainer);
-        cardContainer.append(front, back)
+        cardContainer.append(frontImage, backImage);
+        console.log(shuffledArray[x])
     }
 }
 
-function shuffleArray(array) {
-        var j, x, i;
-        for (i = array.length; i; i -= 1) {
-            j = Math.floor(Math.random() * i);
-            x = array[i - 1];
-            array[i - 1] = array[j];
-            array[j] = x;
-        }
-        return array
+function toggleCard() {
+    var cardClicked = $(this);
+    cardClicked.toggleClass('hide')
 }
 
-// function randomNumber(){
-//     var random_card = cardsArray.length;
-//     random_picture_array = [];
-//     for (x = 0; x < random_card; x++){
-//         random_value = Math.floor(Math.random() * random_card);
-//         random_picture_array.length--;
-//         FrontPicArr.splice(randomNumber, 1);
+
+
+// cardArrayShuffled = shuffleArray(cardsArray);
+
+// function showClickedCard(){
+//     var newDeck = shuffleArray(cardsArray);
+//     // var card = this;
+//     for (var x=0; x < newDeck.length; x++){
+//         card.on('click', function () {
+//             card.removeClass('card-front');
+//             card.css({
+//                 'background-repeat': 'no-repeat',
+//                 'background-size': 'contain',
+//                 // 'background-image': 'url': newDeck.src[x]
+//             })
+//         });
 //     }
+//     console.log(newDeck)
 // }
-
-function showClickedCard(){
-    var newDeck = shuffleArray(cardsArray);
-    return newDeck;
-}
-// function matchedCards(){
-//     if (blah blah === blah blah){
-//         blah blah.remove();
-//     }
+// // function matchedCards(){
+// //     if (blah blah === blah blah){
+// //         blah blah.remove();
+// //     }
+// // }
+//
+// function resetGame(){
+//     readySetGo()
 // }
-
-function resetGame(){
-    readySetGo()
-}
 
